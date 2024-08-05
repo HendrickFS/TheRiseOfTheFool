309c41c8ca28c6a1af27749bb0af73794c8cf791
HendrickFS <hendrickfel@gmail.com>
2024-08-05 14:30:30 -0300

    start slash correction

--- player/attacks/slash.gd
+++ player/attacks/slash.gd
@@ -1,7 +1,8 @@
 extends Area2D
 
 var speed = 500
-var direction = Vector2.ZERO
+var direction = Vector2(1,1)
+
 var sprite
 @onready var player = get_node("/root/world/player")
 
@@ -13,8 +14,8 @@ func _process(delta):
 	position += direction * speed * delta
 
 func initialize(direction_vector, initial_speed):
-	direction = direction_vector.normalized()
+	if(direction_vector != Vector2.ZERO):
+		direction = direction_vector.normalized()
 	speed = initial_speed
-	rotation = direction_vector.angle()
-	
+	rotation = direction.angle()
 	
